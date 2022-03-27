a022169c2a60423e8f2b18133652372c406c6a8a
Kigwa112 <102455272+Kigwa112@users.noreply.github.com>
2022-03-27 13:44:39 +0800

    Create paymaya.php

---
+++ paymaya.php
@@ -0,0 +1,34 @@
+<?php
+		// OH BAT ANDITO KA?
+		// OH BAT ANDITO KA?
+		// OH BAT ANDITO KA?
+		// OH BAT ANDITO KA?
+		// OH BAT ANDITO KA?
+		// OH BAT ANDITO KA?
+    $b="\033[1;35m";
+    $green="\033[1;32m";
+    $yllw="\033[1;33m";
+    $wh="\033[1;94m";
+    $cyn="\033[1;96m";
+    $lred="\033[1;91m";
+    $wht="\033[1;97m";
+    $blhigh = "\033[44m";
+    $defhigh = "\033[49m";
+    $lcyan = "\033[34m";
+    $wh="\033[1;94m";
+    $lred="\033[1;91m";
+	echo $green;
+    echo "\nPaymaya Registration Script";
+    echo "\nPaymaya UNLI REG";
+    echo "\n";
+    //echo "$cyn Version : 2.69.1\n";
+     // OH BAT ANDITO KA?
+    echo $yllw;
+    $number= readline("Enter your Phone Number: +63");
+    sleep(3);
+    echo "\n";
+    echo $yllw;
+    $Otp= readline("-->> OTP: ");
+    sleep(4);
+    echo $green."Registration Success!"."\n\n";
+?>
